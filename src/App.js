0e3303daf5ad100beb9675ff5a181f7affe68f60
Justin Horein <justinhorein@gmail.com>
2021-09-18 17:10:12 -0700

    fin 'Meet another React Component'

--- src/App.js
+++ src/App.js
@@ -21,35 +21,46 @@ const list = [
 ]
 
 function App() {
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-}
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text"/>
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map((item) => {
-          return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
+    </div>
+  )
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>
+              {item.title}
+            </a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.points}</span>
+        </li>
+        );
+      })}
+    </ul>
   );
 }
 
