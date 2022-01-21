5aa864107d3e2d753ba730fc568124e31dcfbf87
Justin Horein <justinhorein@gmail.com>
2022-01-20 18:16:37 -0800

    fin 'Props Handling (Adv)'

--- src/App.js
+++ src/App.js
@@ -57,24 +57,24 @@ const Search = (props) => {
     );
 };
 
-const List = (props) => (
+const List = ({ list }) => (
     <ul>
-      {props.list.map((item) => (
+      {list.map((item) => (
         <Item key={item.objectID} item={item} />
       ))}
     </ul>
 );
 
-const Item = (props) => (
+const Item = ({ item }) => (
   <li>
     <span>
-      <a href={props.item.url}>
-        {props.item.title}
+      <a href={item.url}>
+        {item.title}
       </a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
 )
 
