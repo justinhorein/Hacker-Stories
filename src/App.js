549b8fc24f9e9f863fcc834371eeb90b766c60db
Justin Horein <justinhorein@gmail.com>
2022-08-03 14:19:55 -0700

    fin 'react fragments'

--- src/App.js
+++ src/App.js
@@ -61,11 +61,11 @@ const App = () => {
 
 const Search = (props) => {
     return (
-      <div>
+      <>
         <label htmlFor="search">Search: </label>
         <input id="search" type="text" value={props.search} onChange={props.onSearch}/>
 
-      </div>
+      </>
     );
 };
 
